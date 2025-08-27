d7918bc29781a8d128a9d5d92781c44a123490a2
PopovNikita <popovn@reshenie-soft.ru>
2025-08-27 22:06:38 +0300

    Изменил реализацию StatsClient

--- stats/stats-client/src/main/java/ru/yandex/practicum/client/StatsClient.java
+++ stats/stats-client/src/main/java/ru/yandex/practicum/client/StatsClient.java
@@ -1,14 +1,19 @@
 package ru.yandex.practicum.client;
 
-import ru.yandex.practicum.dto.StatsDto;
-import ru.yandex.practicum.dto.StatsParamsDto;
-import ru.yandex.practicum.dto.StatsResponseDto;
+import ru.yandex.practicum.grpc.stats.action.ActionTypeProto;
+import ru.yandex.practicum.grpc.stats.request.RecommendedEventProto;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface StatsClient {
 
-    List<StatsResponseDto> getAllStats(StatsParamsDto statsParamsDto);
+    void registerUserAction(long eventId, long userId, ActionTypeProto actionType, Instant instant);
 
-    void postStats(StatsDto statsDto);
+    Stream<RecommendedEventProto> getSimilarEvents(long eventId, long userId, int maxResults);
+
+    Stream<RecommendedEventProto> getRecommendationsForUser(long userId, int maxResults);
+
+    Stream<RecommendedEventProto> getEventsInteractionsCount(List<Long> eventIds);
 }
