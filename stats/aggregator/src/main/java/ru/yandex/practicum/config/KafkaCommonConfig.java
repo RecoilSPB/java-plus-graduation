7cc8681ec4cb3ea21e46b7c12102d674bc73b8bb
Попов Никита Михайлович <popovn@reshenie-soft.ru>
2025-09-08 18:42:33 +0300

    fix import

--- stats/aggregator/src/main/java/ru/yandex/practicum/config/KafkaCommonConfig.java
+++ stats/aggregator/src/main/java/ru/yandex/practicum/config/KafkaCommonConfig.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 /**
  * @author PopovN
--- stats/collector/src/main/java/ru/yandex/practicum/config/KafkaConfigProperties.java
+++ stats/collector/src/main/java/ru/yandex/practicum/config/KafkaConfigProperties.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.config;
 
-import lombok.AccessLevel;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.FieldDefaults;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Properties;
